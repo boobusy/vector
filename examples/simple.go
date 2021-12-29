c8f6d04a4e02601ce4eda634f342bbe730913c69
wangguo <booobusy@gmail.com>
2021-12-29 15:07:46 +0800

    add example

---
+++ examples/simple.go
@@ -0,0 +1,23 @@
+//ago:build ignore
+// a+build ignore
+
+package main
+
+import (
+	"fmt"
+	"github.com/boobusy/vector"
+)
+
+func main() {
+	for i := 0; i < 100; i++ {
+		vector.Push(i)
+	}
+
+	fmt.Println(vector.IsEmpty(),vector.Len(),vector.Cap())
+	fmt.Println(vector.Items())
+
+	for !vector.IsEmpty() {
+		fmt.Println(vector.PopBack()) // or fmt.Println(vector.PopFront())
+	}
+
+}
